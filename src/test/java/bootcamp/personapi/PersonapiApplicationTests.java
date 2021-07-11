a173cb798555fd5341a9576b2b73635849807969
Matheus Jacobina <71132039+matheusjacobina-tech@users.noreply.github.com>
2021-07-11 18:27:31 -0300

    commit inicial

---
+++ src/test/java/bootcamp/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
