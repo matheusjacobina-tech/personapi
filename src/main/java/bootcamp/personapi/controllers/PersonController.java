bd140d3296807e3995945ba40bad42ec699191f7
Matheus Jacobina <71132039+matheusjacobina-tech@users.noreply.github.com>
2021-07-15 20:25:15 -0300

    add anotacoes api swagger

--- src/main/java/bootcamp/personapi/controllers/PersonController.java
+++ src/main/java/bootcamp/personapi/controllers/PersonController.java
@@ -5,6 +5,12 @@ import bootcamp.personapi.dto.request.PersonDTO;
 import bootcamp.personapi.dto.response.MessageResponseDTO;
 import bootcamp.personapi.exception.PersonNotFoundException;
 import bootcamp.personapi.services.PersonService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.util.List;
 
+@Api(value = "/api/v1/people", description = "Endpoint API de Gerenciamneto de Pessoas", consumes="application/json")
 @RestController
 @RequestMapping("/api/v1/people")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
@@ -27,29 +34,43 @@ public class PersonController {
 
     private final PersonService personService;
 
+    @ApiOperation(value = "Inserir Pessoa por ID")
+    @ApiResponse(code = 201, message = "Inseriu um registro de pessoa")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO create(@RequestBody @Valid PersonDTO personDTO) {
         return personService.create(personDTO);
     }
 
+    
+    @ApiOperation(value = "Buscar Pessoa por ID")
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "Retorno pessoa por ID", response = PersonDTO.class),
+        @ApiResponse(code = 422, message = "Nenhum pessoa encontado com esse ID")})
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
         return personService.findById(id);
     }
+    
 
+    @ApiOperation(value = "Listar Pessoas")
+    @ApiResponse(code = 200, message = "Retorno pessoa por ID", response = PersonDTO.class, responseContainer = "List")
     @GetMapping
     public List<PersonDTO> listAll() {
         return personService.listAll();
     }
 
+
+    @ApiOperation(value = "Atualizar Pessoa por ID")
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public MessageResponseDTO update(@PathVariable Long id, @RequestBody @Valid PersonDTO personDTO) throws PersonNotFoundException {
         return personService.update(id, personDTO);
     }
 
+
+    @ApiOperation(value = "Deletar Pessoa por ID")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) throws PersonNotFoundException {
